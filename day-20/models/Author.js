17d42f81e95846e3dbac4f227e9f526bf5b04c27
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-01-18 19:09:13 +0700

    edit code Author

---
+++ day-20/models/Author.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+
+const Author = sequelize.define("Author", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = Author;
