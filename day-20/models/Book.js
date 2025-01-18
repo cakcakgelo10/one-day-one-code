f20f3445a3430066203f0b652d87a553a59121a6
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-01-18 19:16:29 +0700

    edit code Book

---
+++ day-20/models/Book.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+const Author = require("./Author");
+
+const Book = sequelize.define("Book", {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+});
+
+// Relasi One - to - Many
+Author.hasMany(Book, { onDelete: "CASCADE" });
+Book.belongsTo(Author);
+
+module.exports = Book;
