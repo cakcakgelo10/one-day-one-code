323e35f32508d5a598fd899c960407bd3fe285c5
cakcakgelo10 <rezaardiansyah343@gmail.com>
2025-01-27 17:05:43 +0700

    add code bookRoutes.js

---
+++ day-21/routes/bookRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { getBooks } = require("../controllers/bookController");
+const router = express.Router();
+
+router.get("/books", getBooks);
+
+module.exports = router;
